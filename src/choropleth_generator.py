b1b110728a7aab464797e106e78b9149c5752fad
Marc Gay-Balmaz <marc.gay-balmaz@unige.ch>
2026-05-28 12:47:19 +0200

    Add: script to generate area comparison values cleanup of choropleth_generator.py and write to file

--- src/choropleth_generator.py
+++ src/choropleth_generator.py
@@ -1,32 +1,25 @@
-
 import plotly.express as px
-import geopandas as gpd
 import json
 
-from shapely import Point
-from shapely.geometry.polygon import Polygon
-
-with open('hectare_2025-04-10 07:30:00_PT3600S.json') as file:
-    hectares = json.load(file)
-with open('geo_ressources/geo_grid_swiss_wgs84_combined_filtered.geojson') as geo_file:
+hectare_filename = "hectare_2025-04-10 07:30:00_PT1800S_split"
+geojson_filename = "geo_ressources/geo_grid_swiss_wgs84_combined_filtered"
+with open(geojson_filename + '.geojson') as geo_file:
 # with open('geo_ressources/geo_romandie_wgs84_PROD_v1.json') as geo_file:
     region_map = json.load(geo_file)
 
 
-df = px.data.election()
-geo_df = gpd.GeoDataFrame.from_features(
-    px.data.election_geojson()["features"]
-).merge(df, on="district").set_index("district")
+with open(hectare_filename + '.json') as file:
+    hectares = json.load(file)
 
 fig = px.choropleth(hectares,
                     geojson=region_map,
                     locations="reli",
                     featureidkey="properties.reli",
-                    range_color=[0,500000000],
-                    color_continuous_midpoint=150,
-                    color="area",
+                    # range_color=[0,500000000],
+                    color_continuous_midpoint=0,
+                    color="diff_max_0",
                     color_continuous_scale='RdYlGn',
                     projection="mercator")
 # fig.update_geos(fitbounds="locations", visible=False)
 fig.update_geos(fitbounds="locations")
-fig.show()
+fig.write_image(file=hectare_filename + '.svg')
---
+++ src/compute_hectare_display_values.py
@@ -0,0 +1,28 @@
+import json
+
+def load_and_update_hectare(hectare_file: str) -> tuple[dict, int]:
+    with open(hectare_file + '.json') as file:
+        hectares = json.load(file)
+
+    highest_cmp = 0
+    for h in hectares:
+        if len(h["area"]) < 2 :
+            continue
+        previous_max, previous_mid, previous_min = h["area"][0]
+        if len(h["area"]) > highest_cmp:
+            highest_cmp = len(h["area"])
+        for i, a in enumerate(h["area"][1:]):
+            max, mid, min = a
+            h["diff_max_"+str(i)] = max - previous_max
+            h["diff_mid_"+str(i)] = mid - previous_mid
+            h["diff_min_"+str(i)] = min - previous_min
+            previous_max, previous_mid, previous_min = a
+    return hectares, highest_cmp
+
+def write_hectare(hectare_file: str, hectares: dict):
+    with open(hectare_file + "_split" + ".json", "w") as out_file:
+        json.dump(hectares, out_file)
+
+if __name__ == "main":
+    hectare_file = "hectare_2025-04-10 07:30:00_PT1800S"
+    write_hectare(hectare_file, load_and_update_hectare(hectare_file)[0])
