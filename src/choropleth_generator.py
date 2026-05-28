2ca00cc13b5bb9658fcd4720ce582700e6dc5528
Marc Gay-Balmaz <marc.gay-balmaz@unige.ch>
2026-05-28 16:00:38 +0200

    Fix: main for both script Add: script generating all images at once

--- src/choropleth_generator.py
+++ src/choropleth_generator.py
@@ -1,9 +1,9 @@
 import plotly.express as px
 import json
 
-def generate_img_from_hectare(hectares, region_map, displayed_value:str, output_filename:str):
-    fig = px.choropleth(hectares,
-                        geojson=region_map,
+def generate_img_from_hectare(data_to_display, region_to_display, displayed_value:str, output_filename:str):
+    fig = px.choropleth(data_to_display,
+                        geojson=region_to_display,
                         locations="reli",
                         featureidkey="properties.reli",
                         # range_color=[0,500000000],
@@ -15,7 +15,7 @@ def generate_img_from_hectare(hectares, region_map, displayed_value:str, output_
     fig.update_geos(fitbounds="locations")
     fig.write_image(file=output_filename + '.svg', scale=100, width=8000, height=8000)
 
-if __name__ == "main":
+if __name__ == "__main__":
     hectare_filename = "hectare_2025-04-10 07:30:00_PT1800S_split"
     geojson_filename = "geo_ressources/geo_grid_swiss_wgs84_combined_filtered"
     with open(geojson_filename + '.geojson') as geo_file:
--- src/compute_hectare_display_values.py
+++ src/compute_hectare_display_values.py
@@ -1,7 +1,7 @@
 import json
 
 def load_and_update_hectare(hectare_file: str) -> tuple[dict, int]:
-    with open(hectare_file + '.json') as file:
+    with open(hectare_file) as file:
         hectares = json.load(file)
 
     highest_cmp = 0
@@ -17,12 +17,12 @@ def load_and_update_hectare(hectare_file: str) -> tuple[dict, int]:
             h["diff_mid_"+str(i)] = mid - previous_mid
             h["diff_min_"+str(i)] = min - previous_min
             previous_max, previous_mid, previous_min = a
-    return hectares, highest_cmp
+    return hectares, highest_cmp - 1
 
 def write_hectare(hectare_file: str, hectares: dict):
     with open(hectare_file + "_split" + ".json", "w") as out_file:
         json.dump(hectares, out_file)
 
-if __name__ == "main":
+if __name__ == "__main__":
     hectare_file = "hectare_2025-04-10 07:30:00_PT1800S"
-    write_hectare(hectare_file, load_and_update_hectare(hectare_file)[0])
+    write_hectare(hectare_file, load_and_update_hectare(hectare_file + '.json')[0])
