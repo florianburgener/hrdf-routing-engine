975a05dee019f276d9803c5476819d86b66edd19
Marc Gay-Balmaz <marc.gay-balmaz@unige.ch>
2026-06-22 16:23:07 +0200

    Add: compute hectare adapted to filtered hectares structure

--- src/compute_hectare_display_values.py
+++ src/compute_hectare_display_values.py
@@ -5,25 +5,40 @@ def load_and_update_hectare(hectare_file: str) -> tuple[list, int]:
         hectares = json.load(file)
 
     highest_cmp = 0
+    filter_base_name = "base"
     for h in hectares:
-        if len(h["area"]) < 2 :
-            continue
-        previous_max, previous_mid, previous_min = h["area"][0]
-        if len(h["area"]) > highest_cmp:
-            highest_cmp = len(h["area"])
-        for i, a in enumerate(h["area"][1:]):
-            max, mid, min = a
-            if i == 0:
-                h["area_"+str(i)+"_max"] = max
-                h["area_"+str(i)+"_mid"] = mid
-                h["area_"+str(i)+"_min"] = min
-            h["area_"+str(i+1)+"_max"] = max
-            h["area_"+str(i+1)+"_mid"] = mid
-            h["area_"+str(i+1)+"_min"] = min
-            h["diff_max_"+str(i)] = previous_max - max
-            h["diff_mid_"+str(i)] = previous_mid - mid
-            h["diff_min_"+str(i)] = previous_min - min
-            previous_max, previous_mid, previous_min = a
+        filters = {}
+        # analyze at first each filter individually in case of multiple times
+        for key, value in h["area"]:
+            previous_max, previous_mid, previous_min = value[0]
+            filters[key] = [value[0]]
+            if len(value) > highest_cmp:
+                highest_cmp = len(value)
+            if len(value) < 2 :
+                continue
+            for i, a in enumerate(value[1:]):
+                max, mid, min = a
+                if i == 0:
+                    h[key + "_area_"+str(i)+"_max"] = max
+                    h[key + "_area_"+str(i)+"_mid"] = mid
+                    h[key + "_area_"+str(i)+"_min"] = min
+                h[key + "_area_"+str(i+1)+"_max"] = max
+                h[key + "_area_"+str(i+1)+"_mid"] = mid
+                h[key + "_area_"+str(i+1)+"_min"] = min
+                h[key + "_diff_max_"+str(i)] = previous_max - max
+                h[key + "_diff_mid_"+str(i)] = previous_mid - mid
+                h[key + "_diff_min_"+str(i)] = previous_min - min
+                previous_max, previous_mid, previous_min = a
+                filters[key].append(a)
+        # Then compare filters to each other
+        for key, value in filters.items():
+            if key == filter_base_name:
+                continue
+            else:
+                for i, (filtered, base) in enumerate(zip(value, filters[filter_base_name])):
+                    for (filtered_val, base_val, metric) in zip(filtered, base, ["max", "mid", "min"]):
+                        h[filter_base_name + "-" + key + "_diff_" + metric + "_" + str(i)] = base_val - filtered_val
+
     return hectares, highest_cmp - 1
 
 def write_hectare(hectare_file: str, hectares: list):
